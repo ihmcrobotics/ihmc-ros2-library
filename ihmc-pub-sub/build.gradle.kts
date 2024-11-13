779dd65f7d3cdad5a57cd3b1fa3bcb16e9599c03
Dexton Anderson <danderson@ihmc.org>
2024-11-13 10:30:47 -0600

    :arrow_up: log-tools 0.6.4, log-tools-plugin 0.6.4

--- ihmc-pub-sub/build.gradle.kts
+++ ihmc-pub-sub/build.gradle.kts
@@ -20,7 +20,7 @@ mainDependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
    api("us.ihmc:euclid:0.22.2")
    api("us.ihmc:ihmc-commons:0.34.0")
-   api("us.ihmc:log-tools:0.6.3")
+   api("us.ihmc:log-tools:0.6.4")
    api("com.sun.xml.bind:jaxb-impl:4.0.5")
 }
 
