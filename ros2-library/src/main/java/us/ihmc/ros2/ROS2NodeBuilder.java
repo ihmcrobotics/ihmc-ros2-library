ce923545564229abe18cba420c52bc9c7b73e6fc
Dexton Anderson <danderson@ihmc.org>
2024-11-21 16:55:38 -0600

    Update javadoc

--- ros2-library/src/main/java/us/ihmc/ros2/ROS2NodeBuilder.java
+++ ros2-library/src/main/java/us/ihmc/ros2/ROS2NodeBuilder.java
@@ -19,7 +19,7 @@ import java.util.Properties;
  *
  * <pre>{@code
  *    ROS2Node node = new ROS2Builder().build("Node");
- *    RealtimeROS2Node realtimeSHMOnlyNode = new ROS2NodeBuilder().specialTransportMode(TransportSpecial.SHARED_MEMORY_ONLY).buildRealtime("SHMNode");
+ *    RealtimeROS2Node realtimeSHMOnlyNode = new ROS2NodeBuilder().specialTransportMode(SpecialTransportMode.SHARED_MEMORY_ONLY).buildRealtime("SHMNode");
  * }</pre>
  * </p>
  */
