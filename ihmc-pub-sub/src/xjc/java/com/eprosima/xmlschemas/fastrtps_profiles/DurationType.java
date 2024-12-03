4f14c40f8d670434c4674b7a8953f39d4a3e0fc5
ihmc-rosie <rosie@ihmc.us>
2024-12-03 22:43:06 +0000

    Rebuild natives for Linux

--- ihmc-pub-sub/src/xjc/java/com/eprosima/xmlschemas/fastrtps_profiles/DurationType.java
+++ ihmc-pub-sub/src/xjc/java/com/eprosima/xmlschemas/fastrtps_profiles/DurationType.java
@@ -71,8 +71,8 @@ import jakarta.xml.bind.annotation.XmlType;
 public class DurationType {
 
     @XmlElementRefs({
-        @XmlElementRef(name = "nanosec", namespace = "http://www.eprosima.com/XMLSchemas/fastRTPS_Profiles", type = JAXBElement.class, required = false),
-        @XmlElementRef(name = "sec", namespace = "http://www.eprosima.com/XMLSchemas/fastRTPS_Profiles", type = JAXBElement.class, required = false)
+        @XmlElementRef(name = "sec", namespace = "http://www.eprosima.com/XMLSchemas/fastRTPS_Profiles", type = JAXBElement.class, required = false),
+        @XmlElementRef(name = "nanosec", namespace = "http://www.eprosima.com/XMLSchemas/fastRTPS_Profiles", type = JAXBElement.class, required = false)
     })
     protected List<JAXBElement<String>> secOrNanosec;
 
