0b9a21d4169957387f6a8836da5fca3093bc2342
Dexton Anderson <danderson@ihmc.org>
2025-02-12 16:19:20 -0600

    Small cleanup

--- ros2-library/src/test/java/us/ihmc/ros2/ROS2SubscriptionTest.java
+++ ros2-library/src/test/java/us/ihmc/ros2/ROS2SubscriptionTest.java
@@ -1,22 +1,15 @@
 package us.ihmc.ros2;
 
-import org.gradle.internal.impldep.org.junit.Assume;
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assumptions.*;
+import org.junit.jupiter.api.Timeout;
 import std_msgs.msg.dds.ByteMultiArray;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.ros2.ROS2NodeBuilder.SpecialTransportMode;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,74 +19,6 @@ public class ROS2SubscriptionTest
    private static final Random RANDOM = new Random(349473937);
    private static final ByteMultiArray BIG_MESSAGE = generateBigMessage(100);
 
-   @Test
-   public void testRaceCondition() throws InterruptedException
-   {
-      final int subscriberCount = 2;
-      final int messagesToPublish = 10000;
-      final int expectedMessageCount = subscriberCount * messagesToPublish;
-
-      ROS2Topic<ByteMultiArray> topic = new ROS2Topic<>().withType(ByteMultiArray.class).withSuffix("test_topic").withQoS(ROS2QosProfile.RELIABLE());
-      ROS2NodeBuilder nodeBuilder = new ROS2NodeBuilder().specialTransportMode(SpecialTransportMode.UDPV4_ONLY).useSharedMemory(false);
-
-      // Build publisher node and publisher
-      ROS2Node publisherNode = nodeBuilder.build("publisher_node");
-      ROS2Publisher<ByteMultiArray> publisher = publisherNode.createPublisher(topic);
-      AtomicInteger totalPublishedMessages = new AtomicInteger();
-
-      // Build subscribers and message listener
-      List<ROS2Node> subscriberNodes = new ArrayList<>();
-      Map<ROS2Node, ROS2Subscription<ByteMultiArray>> subscriptions = new HashMap<>();
-      AtomicInteger totalReceivedMessages = new AtomicInteger();
-      for (int i = 0; i < subscriberCount; i++)
-         subscriberNodes.add(nodeBuilder.build("subscriber_node_" + i)); // TODO: ROS2NodeBuilder bug? Doesn't update the name when it prints
-      Set<String> subscribersThatGotData = new HashSet<>();
-      for (ROS2Node subscriberNode : subscriberNodes)
-      {
-         subscriptions.put(subscriberNode, subscriberNode.createSubscription(topic, subscriber ->
-         {
-            totalReceivedMessages.incrementAndGet();
-            subscribersThatGotData.add(subscriber.getGuid().toString());
-         }));
-      }
-
-      Thread publisherThread = new Thread(() ->
-      {
-         for (int i = 0; i < messagesToPublish; i++)
-         {
-            publisher.publish(BIG_MESSAGE);
-            totalPublishedMessages.incrementAndGet();
-            ThreadTools.sleep(1);
-         }
-      });
-
-//      Thread stopThread = new Thread(() ->
-//      {
-//         while (totalPublishedMessages.get() <= messagesToPublish)
-//         {
-//            ThreadTools.sleep(1);
-//            subscriptions.forEach((ros2Node, subscription) -> subscription.remove());
-//         }
-//      });
-
-      publisherThread.start();
-      publisherThread.join();
-
-      // Check that all the subscribers got data
-      assertEquals(subscriberCount, subscribersThatGotData.size());
-      // Check that all the messages were received by all the subscribers
-      assertEquals(expectedMessageCount, totalReceivedMessages.get());
-
-      // Destroy subscriptions and subscriber nodes
-      subscriptions.forEach((subscriberNode, subscription) -> subscription.remove());
-      for (ROS2Node subscriberNode : subscriberNodes)
-         subscriberNode.destroy();
-
-      // Destroy publisher and publisher node
-      publisher.remove();
-      publisherNode.destroy();
-   }
-
    @RepeatedTest(500)
    public void testPublishSubscribe()
    {
@@ -101,7 +26,7 @@ public class ROS2SubscriptionTest
 
       AtomicInteger receivedData = new AtomicInteger(0);
 
-      ROS2Topic<ByteMultiArray> topic = new ROS2Topic<>().withType(ByteMultiArray.class).withSuffix("test_topic");//.withQoS(ROS2QosProfile.RELIABLE());
+      ROS2Topic<ByteMultiArray> topic = new ROS2Topic<>().withType(ByteMultiArray.class).withSuffix("test_topic");
       ROS2NodeBuilder builder = new ROS2NodeBuilder().specialTransportMode(SpecialTransportMode.UDPV4_ONLY);
       ROS2Node publisherNode = builder.build("publisher_node");
       ROS2Node subscriberNode = builder.build("subscriber_node");
@@ -145,9 +70,10 @@ public class ROS2SubscriptionTest
    }
 
    @Test
+   @Timeout(10)
    public void testRemoveDeadlock()
    {
-      ROS2Topic<ByteMultiArray> topic = new ROS2Topic<>().withType(ByteMultiArray.class).withSuffix("test_topic").withQoS(ROS2QosProfile.BEST_EFFORT());
+      ROS2Topic<ByteMultiArray> topic = new ROS2Topic<>().withType(ByteMultiArray.class).withSuffix("test_topic");
       ROS2NodeBuilder builder = new ROS2NodeBuilder().specialTransportMode(SpecialTransportMode.UDPV4_ONLY);
       ROS2Node publisherNode = builder.build("publisher_node");
       ROS2Node subscriberNode = builder.build("subscriber_node");
@@ -167,7 +93,6 @@ public class ROS2SubscriptionTest
                ROS2Subscription<ByteMultiArray> subscription = subscriberNode.createSubscription(topic, subscriber ->
                {
                   ByteMultiArray data = subscriber.takeNextData();
-                  System.out.println(data);
                });
 
                ThreadTools.park(RANDOM.nextDouble(0.1));
