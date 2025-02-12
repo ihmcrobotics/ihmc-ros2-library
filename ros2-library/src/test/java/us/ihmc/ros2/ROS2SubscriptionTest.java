d0ea38fd01676a7a6ab5751d602688a57f498335
Dexton Anderson <danderson@ihmc.org>
2025-02-12 14:09:53 -0600

    More test changes

--- ros2-library/src/test/java/us/ihmc/ros2/ROS2SubscriptionTest.java
+++ ros2-library/src/test/java/us/ihmc/ros2/ROS2SubscriptionTest.java
@@ -1,6 +1,6 @@
 package us.ihmc.ros2;
 
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import std_msgs.msg.dds.ByteMultiArray;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.ros2.ROS2NodeBuilder.SpecialTransportMode;
@@ -21,11 +21,12 @@ public class ROS2SubscriptionTest
    private static final Random RANDOM = new Random(349473937);
    private static final ByteMultiArray BIG_MESSAGE = generateBigMessage(100);
 
-   @RepeatedTest(50)
+   @Test
    public void testRaceCondition() throws InterruptedException
    {
       final int subscriberCount = 2;
-      final int messagesToPublish = 100;
+      final int messagesToPublish = 10000;
+      final int expectedMessageCount = subscriberCount * messagesToPublish;
 
       ROS2Topic<ByteMultiArray> topic = new ROS2Topic<>().withType(ByteMultiArray.class).withSuffix("test_topic").withQoS(ROS2QosProfile.RELIABLE());
       ROS2NodeBuilder nodeBuilder = new ROS2NodeBuilder().specialTransportMode(SpecialTransportMode.UDPV4_ONLY).useSharedMemory(false);
@@ -33,6 +34,7 @@ public class ROS2SubscriptionTest
       // Build publisher node and publisher
       ROS2Node publisherNode = nodeBuilder.build("publisher_node");
       ROS2Publisher<ByteMultiArray> publisher = publisherNode.createPublisher(topic);
+      AtomicInteger totalPublishedMessages = new AtomicInteger();
 
       // Build subscribers and message listener
       List<ROS2Node> subscriberNodes = new ArrayList<>();
@@ -41,33 +43,40 @@ public class ROS2SubscriptionTest
       for (int i = 0; i < subscriberCount; i++)
          subscriberNodes.add(nodeBuilder.build("subscriber_node_" + i)); // TODO: ROS2NodeBuilder bug? Doesn't update the name when it prints
       Set<String> subscribersThatGotData = new HashSet<>();
-      NewMessageListener<ByteMultiArray> messageListener = subscriber ->
-      {
-         totalReceivedMessages.incrementAndGet();
-         subscribersThatGotData.add(subscriber.getGuid().toString());
-      };
       for (ROS2Node subscriberNode : subscriberNodes)
-         subscriptions.put(subscriberNode, subscriberNode.createSubscription(topic, messageListener));
+      {
+         subscriptions.put(subscriberNode, subscriberNode.createSubscription(topic, subscriber ->
+         {
+            totalReceivedMessages.incrementAndGet();
+            subscribersThatGotData.add(subscriber.getGuid().toString());
+         }));
+      }
 
       Thread publisherThread = new Thread(() ->
       {
          for (int i = 0; i < messagesToPublish; i++)
          {
             publisher.publish(BIG_MESSAGE);
+            totalPublishedMessages.incrementAndGet();
+            ThreadTools.sleep(1);
          }
       });
+
+//      Thread stopThread = new Thread(() ->
+//      {
+//         while (totalPublishedMessages.get() <= messagesToPublish)
+//         {
+//            ThreadTools.sleep(1);
+//            subscriptions.forEach((ros2Node, subscription) -> subscription.remove());
+//         }
+//      });
 
       publisherThread.start();
       publisherThread.join();
 
-      // Wait some time for the subscriber callback to run
-      // The messages have to go over the network interface, etc
-      ThreadTools.park(0.1);
-
       // Check that all the subscribers got data
       assertEquals(subscriberCount, subscribersThatGotData.size());
       // Check that all the messages were received by all the subscribers
-      int expectedMessageCount = subscriberCount * messagesToPublish;
       assertEquals(expectedMessageCount, totalReceivedMessages.get());
 
       // Destroy subscriptions and subscriber nodes
