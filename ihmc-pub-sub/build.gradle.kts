82b9b2f2dfa0d2a90c874ad28eb8161202ac5141
ds58 <30220598+ds58@users.noreply.github.com>
2025-03-05 11:56:17 -0600

    :arrow_up: ihmc-native-library-loader 2.0.4 (#63)

--- ihmc-pub-sub/build.gradle.kts
+++ ihmc-pub-sub/build.gradle.kts
@@ -15,7 +15,7 @@ ihmc {
 }
 
 mainDependencies {
-   api("us.ihmc:ihmc-native-library-loader:2.0.3")
+   api("us.ihmc:ihmc-native-library-loader:2.0.4")
    api("net.sf.trove4j:trove4j:3.0.3")
    api("us.ihmc:euclid:0.22.3")
    api("us.ihmc:ihmc-commons:0.35.1")
